b5d6634b12e2b2a56f97ec0ef93d178fa57e95a8
SelnoxInfotech <117904460+SelnoxInfotech@users.noreply.github.com>
2022-12-06 10:25:03 +0530

    Update views.py

--- Ecommerce/AdminPanel/views.py
+++ Ecommerce/AdminPanel/views.py
@@ -698,8 +698,8 @@ class CouponFilter(FilterSet):
     max_value = NumberFilter(name='value', lookup_expr='lte')
 
     class Meta:
-        model = apps.get_model('coupons.Coupon')
-        fields = ['user', 'bound', 'type', 'min_value', 'max_value']
+        model = Coupon
+        fields = '__all__'
         
 from django.conf import settings
 from django.contrib.auth.decorators import user_passes_test
